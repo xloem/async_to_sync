ea5f12819b656726a4d3da891a459db2340bc747
0xloem <0xloem@gmail.com>
2021-01-07 09:57:22 -0500

    initial import; package setup taken from bleak

---
+++ README.rst
@@ -0,0 +1,23 @@
+=============
+async_to_sync
+=============
+
+Convert async coroutine functions to normal ones.
+
+Installation
+------------
+
+.. code-block:: bash
+
+    $ pip install async_to_sync
+
+Usage
+-----
+
+.. code-block:: python
+
+    import async_to_sync as sync
+
+    sync_object = sync.async_methods(async_class())
+    sync_function = sync.function(async_class().method)
+    sync_function = sync.coroutine(async_class().method())
---
+++ async_to_sync/__init__.py
@@ -0,0 +1,47 @@
+import asyncio
+import atexit
+import threading
+
+_loop = None
+def get_event_loop():
+    global _loop, _thread
+    if _loop is None:
+        _loop = asyncio.get_event_loop()
+        _thread = threading.Thread(target=_loop.run_forever)
+        _thread.start()
+    elif _thread is None:
+        _thread = threading.Thread(target=_loop.run_forever)
+        _thread.start()
+    return _loop
+def start():
+    get_event_loop()
+def stop():
+    global _loop, _thread
+    if _loop is not None:
+        _loop.call_soon_threadsafe(_loop.stop)
+        _thread.join()
+        _thread = None
+
+def coroutine(coroutine, loop = None):
+    if loop is None:
+        loop = get_event_loop()
+    future = asyncio.run_coroutine_threadsafe(coroutine, loop)
+    result = future.result()
+    return result
+
+def function(function, loop = None):
+    def call(*params, **kwparams):
+        async_coroutine = function(*params, **kwparams)
+        return coroutine(async_coroutine, loop)
+    return call
+
+class async_methods:
+    def __init__(self, object, loop = None):
+        self.__object = object
+        self.__loop = loop
+    def __getattr__(self, name):
+        result = getattr(self.__object, name)
+        if asyncio.iscoroutinefunction(result):
+            return function(result, self.__loop)
+        else:
+            return result
